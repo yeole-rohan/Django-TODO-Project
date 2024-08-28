69847c559ccf62384f2ebaac688251198b6a981b
rohan yeole <rayeole@gail.com>
2024-08-28 10:04:13 +0530

    Creating an Update View for TODO Items

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import todo_create, todo_list
+from .views import todo_create, todo_list, TodoUpdateView
 
 urlpatterns = [
    path('create/', todo_create, name='todo_create'),  # FBV
    path('', todo_list, name='todo_list'),  # FBV for listing TODO items
+   path('update-cbv/<int:pk>/', TodoUpdateView.as_view(), name='todo_update_cbv'),  # CBV for updating TODO
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .forms import TodoItemForm
 from .models import TodoItem
+from django.views.generic.edit import UpdateView
 
 def todo_create(request):
     if request.method == 'POST':
@@ -17,3 +19,10 @@ def todo_create(request):
 def todo_list(request):
     todos = TodoItem.objects.all()  # Fetch all TODO items from the database
     return render(request, 'todo/todo_list.html', {'todos': todos})
+
+
+class TodoUpdateView(UpdateView):
+    model = TodoItem
+    template_name = 'todo/todo_form.html'
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('todo:todo_list')  # Redirect to the list view after updating
