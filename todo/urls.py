20487a15f8236de38972ad010ed8320ee5f58607
rohan yeole <rayeole@gail.com>
2024-08-28 10:16:47 +0530

    Implementing Move to Bin Feature

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todo_create, todo_list, TodoUpdateView
+from .views import todo_create, todo_list, TodoUpdateView, delete_todo
 
 urlpatterns = [
    path('create/', todo_create, name='todo_create'),  # FBV
    path('', todo_list, name='todo_list'),  # FBV for listing TODO items
    path('update-cbv/<int:pk>/', TodoUpdateView.as_view(), name='todo_update_cbv'),  # CBV for updating TODO
+   path('delete-todo/<int:pk>/', delete_todo, name='delete_todo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse_lazy
 from .forms import TodoItemForm
 from .models import TodoItem
 from django.views.generic.edit import UpdateView
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
 
 def todo_create(request):
     if request.method == 'POST':
@@ -26,3 +29,10 @@ class TodoUpdateView(UpdateView):
     template_name = 'todo/todo_form.html'
     fields = ['title', 'description', 'completed']
     success_url = reverse_lazy('todo:todo_list')  # Redirect to the list view after updating
+
+
+@require_http_methods(["DELETE"])
+def delete_todo(request, pk):
+    todo = get_object_or_404(TodoItem, pk=pk)
+    todo.delete()
+    return JsonResponse({'status': 'success'})
